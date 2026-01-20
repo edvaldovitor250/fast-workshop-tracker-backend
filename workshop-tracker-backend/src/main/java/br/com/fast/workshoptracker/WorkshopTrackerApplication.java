44d304a9967dfab151f056c1239f28abfa195849
Edvaldo Vitor De Castro Souza <116117189+edvaldovitor250@users.noreply.github.com>
2026-01-20 19:02:51 -0300

    feat: initial Spring Boot project structure

---
+++ workshop-tracker-backend/src/main/java/br/com/fast/workshoptracker/WorkshopTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fast.workshoptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopTrackerApplication.class, args);
+	}
+
+}
---
+++ workshop-tracker-backend/src/test/java/br/com/fast/workshoptracker/WorkshopTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fast.workshoptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
